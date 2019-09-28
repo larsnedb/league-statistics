fc6130d2345ee6d42ac4f20389ad2f92f554e12f
larsnedb <lars.nedberg@gmail.com>
2019-09-28 14:50:04 +0200

    Maps single match to two separate objects for home and away

--- src/app/navigation/teams-view/teams-view.component.ts
+++ src/app/navigation/teams-view/teams-view.component.ts
@@ -1,7 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {MatchService} from '../../services/match.service';
-import {MatchReport} from '../../models/match-report.model';
-import {LeagueEntry} from '../../models/league-entry.model';
+import {MatchForTeam} from '../../models/match-for-team.model';
 
 @Component({
   selector: 'app-teams-view',
@@ -13,57 +12,12 @@ export class TeamsViewComponent implements OnInit {
   constructor(private matchService: MatchService) {
   }
 
-  // matches: MatchReport[];
+  matches: MatchForTeam[];
   columnsToDisplay: string[] = ['teamName', 'matches', 'victories', 'victory-penalty', 'loss-penalty', 'loss', 'goalsFor',
     'goalsAgainst', 'goalDiff', 'points'];
-  league: LeagueEntry = {};
-  standings: LeagueEntry[] = [];
-  match: MatchReport;
-
 
   ngOnInit() {
-    this.match = this.matchService.getMatch(1331202005);
-
-
-    const league: LeagueEntry = {};
-    // this.matches.forEach(match => {
-
-    const homeTeam = this.match.info[0].HomeTeamName;
-    const awayTeam = this.match.info[0].AwayTeamName;
-    const endResult = this.match.info[0].EndResult.split('-');
-
-
-    league.teamName = homeTeam;
-
-    if (!league.stats) {
-      this.initStats(league);
-    }
-
-    if (endResult[0] > endResult[1]) {
-      league.stats.goalsAgainst += parseInt(endResult[1], 10);
-      league.stats.goalsFor += parseInt(endResult[0], 10);
-      league.stats.points += 3;
-      league.stats.matchesPlayed++;
-      league.stats.victories++;
-
-    } else if (endResult[0] < endResult[1]) {
-
-
-    }
-    this.standings.push(league);
-
+    this.matches = this.matchService.getAllMatchesList();
   }
 
-  private initStats(league: LeagueEntry) {
-    league.stats = {
-      matchesPlayed: 0,
-      victories: 0,
-      penaltyVictories: 0,
-      losses: 0,
-      lossPenalties: 0,
-      goalsFor: 0,
-      goalsAgainst: 0,
-      points: 0
-    };
-  }
 }
