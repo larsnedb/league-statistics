ead8c37b44020899b9712bde8853ac8940fbab44
larsnedb <lars.nedberg@gmail.com>
2019-09-28 14:51:14 +0200

    deleted empty test files and small cleanup

---
+++ src/app/models/match-result.enum.ts
@@ -0,0 +1,6 @@
+export const enum MatchResult {
+  WIN = 3,
+  WIN_PENS = 2,
+  LOSS_PENS = 1,
+  LOSS = 0,
+}
--- src/app/pipes/date-norwegian.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {DateNorwegianPipe} from './date-norwegian.pipe';
-
-describe('DateNorwegianPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DateNorwegianPipe('nb-NO');
-    expect(pipe).toBeTruthy();
-  });
-});
