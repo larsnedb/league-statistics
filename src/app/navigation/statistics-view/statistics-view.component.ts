a97fd02be55f178a996ae2a2f90bb6fe98b5924c
larsnedb <lars.nedberg@gmail.com>
2019-10-01 20:46:15 +0200

    Added fileReader for reading single data file and displaying in statistics view

--- src/app/navigation/statistics-view/statistics-view.component.ts
+++ src/app/navigation/statistics-view/statistics-view.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FileReaderService} from '../../services/file-reader.service';
+import {MatchReport} from '../../models/match-report.model';
+import {ComplexMatchReport} from '../../models/complex-match-report.model';
 
 @Component({
   selector: 'app-statistics-view',
@@ -11,8 +13,21 @@ export class StatisticsViewComponent implements OnInit {
   constructor(private fileReader: FileReaderService) {
   }
 
+  match: MatchReport;
+
   ngOnInit() {
-    this.fileReader.readFiles();
+    this.fileReader.readFiles().subscribe(result => {
+      this.match = this.simplifyMatchObject(result);
+    });
+  }
+
+  private simplifyMatchObject(matchReport: ComplexMatchReport): MatchReport {
+    return {
+      info: matchReport.info[0],
+      pens: matchReport.pens[0],
+      Goals: matchReport.Goals[0],
+      participants: matchReport.participants[0]
+    };
   }
 
 }
--- src/app/services/file-reader.service.ts
+++ src/app/services/file-reader.service.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {MatchReport} from '../models/match-report.model';
+import {Observable} from 'rxjs';
+import {ComplexMatchReport} from '../models/complex-match-report.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +14,8 @@ export class FileReaderService {
 
   match: MatchReport;
 
-  readFiles() {
-    this.http.get<MatchReport>('assets/match_reports/2018-09-15_Gre_Lil.json', {responseType: 'json'})
-      .subscribe(result => {
-        this.match = result;
-        console.log(this.match);
-      });
+  readFiles(): Observable<ComplexMatchReport> {
+    return this.http.get<ComplexMatchReport>('assets/match_reports/2018-09-15_Gre_Lil.json', {responseType: 'json'});
   }
 }
 
