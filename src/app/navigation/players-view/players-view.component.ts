82322a249e008773e4bc534063dc251f46107b0f
larsnedb <lars.nedberg@gmail.com>
2019-10-13 15:51:55 +0200

    Basic player statistic view showing player and number of points

--- src/app/navigation/players-view/players-view.component.ts
+++ src/app/navigation/players-view/players-view.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatchService} from '../../services/match.service';
+import {MatchInfo} from '../../models/match-info.model';
+import {MatchParticipants} from '../../models/match-participants.model';
+import {MatchPlayer} from '../../models/match-player.model';
 
 @Component({
   selector: 'app-players-view',
@@ -7,9 +11,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayersViewComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private matchService: MatchService) {
   }
 
+  matchInfo: MatchInfo;
+  participants: MatchParticipants;
+  columnsToDisplay = ['playername', 'points', 'goals', 'assists'];
+
+  homePlayers: MatchPlayer[];
+
+
+  ngOnInit() {
+    const match = this.matchService.getMatch(1331202005);
+
+    this.matchInfo = match.info;
+    this.participants = match.participants;
+
+    this.homePlayers = match.participants.HomePlayers
+      .sort(this.comparePlayersSortByPoints);
+  }
+
+  private comparePlayersSortByPoints(a: MatchPlayer, b: MatchPlayer) {
+    if (a.Points > b.Points) {
+      return -1;
+    } else if (a.Points === b.Points) {
+      if (a.Goals > b.Goals) {
+        return -1;
+      } else if (a.Goals === b.Goals) {
+        if (a.Assists > b.Assists) {
+          return -1;
+        }
+      }
+      return 1;
+    }
+  }
 }
