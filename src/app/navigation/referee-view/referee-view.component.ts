ecfebae805f390c026b97cff7e1aa1fdc88f1c65
Lars <you@example.com>
2020-05-11 21:34:48 +0200

    Displaying number of matches per referee

--- src/app/navigation/referee-view/referee-view.component.ts
+++ src/app/navigation/referee-view/referee-view.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {MatchService} from '../../services/match.service';
+import {Referee} from '../../models/referee.model';
 
 @Component({
   selector: 'app-referee-view',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RefereeViewComponent implements OnInit {
 
-  constructor() { }
+  refData: Map<string, number> = new Map<string, number>();
+  totalMatches = 0;
 
-  ngOnInit() {
+  constructor(private matchService: MatchService) {
   }
 
+  ngOnInit() {
+    const matches = this.matchService.getAllMatches();
+
+    matches.forEach(match => {
+      match.info.Referees.forEach(referee => {
+        this.getRefName(referee);
+      });
+    });
+  }
+
+  private getRefName(referee: Referee) {
+    this.totalMatches++;
+    const refName = referee.Name.split('Dommer ')[1];
+    if (this.refData.has(refName)) {
+      let numberOfMatches = this.refData.get(refName);
+      numberOfMatches += 1;
+      this.refData.set(refName, numberOfMatches);
+    } else {
+      this.refData.set(refName, 1);
+    }
+  }
 }
